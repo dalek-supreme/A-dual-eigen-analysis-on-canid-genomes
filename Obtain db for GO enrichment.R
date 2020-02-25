126de2722dbd040911f8f78aadf3a867f2e79e7c
dalek-supreme <leosun.ny@icloud.com>
2020-02-25 22:44:03 +0800

    Update Obtain db for GO enrichment.R

--- Obtain db for GO enrichment.R
+++ Obtain db for GO enrichment.R
@@ -4,6 +4,10 @@ hub <- AnnotationHub()
 sql_id_canfam<-names(hub[hub$species=='Canis familiaris'&hub$rdataclass=='OrgDb'])
 # Canisfam<-hub[["AH75736"]]
 Canisfam<-hub[[sql_id_canfam]]
-bitr(keys(Canisfam)[1], 'ENTREZID', c("REFSEQ", "GO", "ONTOLOGY"), Canisfam)
+
+
+# translate ID to Ontology terms
+#library(clusterProfiler)
+#bitr(keys(Canisfam)[1], 'ENTREZID', c("REFSEQ", "GO", "ONTOLOGY"), Canisfam)
 
 #save(Canisfam,file="Canisfam.Rdata")
