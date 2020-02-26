e9019ffbdb4b3cca0b94b00ec7e5c51da006f6cd
dalek-supreme <leosun.ny@icloud.com>
2020-02-26 23:33:29 +0800

    Get Databases
    1. Gene Ontology Pathes w/ AnnotationHub
    2. Gene ENTRENZIDs corresponding to the SNP sites

--- Obtain db for GO enrichment.R
+++
@@ -1,9 +0,0 @@
-require(AnnotationHub)
-hub <- AnnotationHub()
-# hub[hub$species=='Canis familiaris'&hub$rdataclass=='OrgDb']
-sql_id_canfam<-names(hub[hub$species=='Canis familiaris'&hub$rdataclass=='OrgDb'])
-# Canisfam<-hub[["AH75736"]]
-Canisfam<-hub[[sql_id_canfam]]
-
-GO.data<-select(Canisfam,keys(Canisfam),c("GO"))
-save(GO.data,file="GO.data.Rdata")
