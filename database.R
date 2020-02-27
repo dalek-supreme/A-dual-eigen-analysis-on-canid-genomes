306991a1cea804c2387b4b4454f1df64e50f514e
Xingzhi Sun <xs2365@columbia.edu>
2020-02-27 15:51:45 +0800

    in utils: get counts and sums of loadings in each gene

--- database.R
+++ database.R
@@ -7,7 +7,7 @@
 # currently just for one chromosome.
 library("biomaRt")
 ensembl = useMart("ensembl",dataset="cfamiliaris_gene_ensembl")
-gene.data <- getBM(attributes=c("start_position","end_position","go_id","name_1006","definition_1006","go_linkage_type","namespace_1003"),
+gene.data <- getBM(attributes=c("ensembl_gene_id","entrezgene_id","start_position","end_position","go_id","name_1006","definition_1006","go_linkage_type","namespace_1003"),
 	 		filters='chromosome_name',
 	 		values=chr_t,
 	 		mart=ensembl)
