c5e34e9ce4ab5ed3d5e2252a5573395315e0d0b8
Xingzhi Sun <xs2365@columbia.edu>
2020-02-27 20:05:49 +0800

    Update utils.R

--- utils.R
+++ utils.R
@@ -7,7 +7,7 @@ prj <- function(vec_from,vec_to) {
 
 
 # count the SNP sites per gene
-gene.stats <- function(layer,r_mtx_from_svd,gene.data) {
+get.gene.snp.counts_and_sums <- function(layer,r_mtx_from_svd,gene.data) {
     result.data <- data.frame(
         ensembl_gene_id = unique(gene.data$ensembl_gene_id),
         count = array(0,length(unique(gene.data$ensembl_gene_id))),
@@ -36,6 +36,7 @@ gene.stats <- function(layer,r_mtx_from_svd,gene.data) {
             pos[coordinates[site_num]] <=
             unique(gene.data[gene.data$ensembl_gene_id==gene,]$end_position)
             ){
+                #print(paste("in gene:",gene,"rank =",rank(r_mtx_from_svd[,layer])[site_num],sep=" "))
                 #print(paste("found in gene: ",gene," at site: ",site_num,". ", site_num/dim(r_mtx_from_svd)[1]*100,"% completed.",sep=""))
                 snp.loading.count <- snp.loading.count + 1
                 snp.loading.sum <- snp.loading.sum + r_mtx_from_svd[site_num,layer]
@@ -50,20 +51,75 @@ gene.stats <- function(layer,r_mtx_from_svd,gene.data) {
     return(result.data)
 }
 
-enrichment.wilcoxon <- function()
+# take intersection with the genes with variations,
+# as required by the wilcoxon enrichment analysis
+get.gene.data.remove0s <- function(gene.stats,gene.data){
+    zeros.index <-
+        gene.data$ensembl_gene_id %in% 
+            gene.stats[gene.stats$count==0,]$ensembl_gene_id
+    return(gene.data[!zeros.index,])
+}
+
+#gene.stats[(gene.stats$ensembl_gene_id %in% unique(get.gene.data.remove0s(gene.stats,gene.data)$ensembl_gene_id)),]$count
+#gene.stats[!(gene.stats$ensembl_gene_id %in% unique(get.gene.data.remove0s(gene.stats,gene.data)$ensembl_gene_id)),]$count
+
+# removes zeros. takes average of loadings. and sorts.
+get.gene.stats.sorted <- function(gene.stats){
+    result.data <- data.frame(
+        ensembl_gene_id=array("",length(gene.stats[gene.stats$count!=0,]$ensembl_gene_id)),
+        avg_loading=array(0,length(gene.stats[gene.stats$count!=0,]$ensembl_gene_id)),
+        stringsAsFactors = F
+    )
+    avg <- gene.stats[gene.stats$count!=0,]$sum/gene.stats[gene.stats$count!=0,]$count
+    result.data$avg_loading <- sort(avg)
+    result.data$ensembl_gene_id <- 
+        gene.stats[gene.stats$count!=0,]$ensembl_gene_id[order(avg)]
+    return(result.data)
+}
+
+# get subset for analysis with index from index.start to index.end
+get.gene.subset <- function(gene.stats.sorted, index.start, index.end){
+    return(gene.stats.sorted[index.start:index.end,])
+}
+
+#test:
+#gene.subset<-get.gene.subset(get.gene.stats.sorted(gene.stats),1,100)
+
+# does wilcoxon rank sum test on given subset w/ loadings
+enrichment.wilcoxon <- function(gene.subset,gene.data){
+    enrichment.result <- data.frame(
+        go_id = array("",length(unique(gene.data$go_id))),
+        p_value = array("",length(unique(gene.data$go_id))),
+        stringsAsFactors = F
+    )
+    pathway.ids <- unique(gene.data$go_id)
+    pathway.pval <- sapply(pathway.ids, function(x){
+        index <- gene.data$go_id==x
+        pathway.genes <- gene.data[index,]$ensembl_gene_id
+        index <- gene.subset$ensembl_gene_id %in% pathway.genes
+        loadings.in_pathway <- gene.subset[index,]$avg_loading
+        loadings.out_of_pathway <- gene.subset[!index,]$avg_loading
+        if (length(pathway.genes) >= 6){ # Question: why is this 6 and is this necessary and does it vary by different tests?
+            wilcox.test(loadings.in_pathway, loadings.out_of_pathway, alternative = "greater")$p.val
+        }
+    })
+}
+
 enrichment.chi_square <- function()
 enrichment.blahblah <- function()
 
+# a generic function for enrichment
+# does <enrichment.test> on given subset w/ loadings
+# where <enrichment.test> is any statistical two-sample test,
+# such as <wilcox.test> and the chi-square test, etc.
+enrichment <- function(gene.subset,gene.data,enrichment.test){
 
+}
 
-pathway.id <- unique(GO.data[,2])
-
-
-enrichment.wilcoxon <- function(experiment.data, pathway.data) {
-
-    pathway.pval <- sapply(pathway.id, function(x){
-        
-    })
-    # result<-data.frame(...)
-    # return(result)
+# sort the enrichment result by p-value,
+# then output the terms with the <num_terms> smallest p-values
+enrichment.output <- function(enrichment.result, gene.data, num_terms){
+    output <- data.frame(
+        # information included in output
+    )
 }
