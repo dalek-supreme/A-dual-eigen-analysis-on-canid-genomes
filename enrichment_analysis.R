dc6a0a218f4158b70cc928914055f057bdf94c47
Xingzhi Sun <xs2365@columbia.edu>
2020-02-28 21:18:03 +0800

    Update enrichment_analysis.R

--- enrichment_analysis.R
+++ enrichment_analysis.R
@@ -1,10 +1,10 @@
 # enrichment analysis on loadings
-source(utils.R)
-source(database.R)
 
-#setwd("") # set to the correct working dir
+setwd("~") # set to the correct working dir
 
-load("gene.data.Rdata")
+source("database.R")
+#source("utils.R")
+#load("gene.data.Rdata")
 
 # Wilcoxon Rank Sum Enrichment
 layers <- 1:10
@@ -13,7 +13,7 @@ num_terms <- 50
 # largest 100 genes
 
 setwd("~/results_wilcox/large100")
-filename <- paste("output_wilcox_chr=",chr_t,".csv",sep="")
+filename <- paste("output_wilcox_chr=",chr_t,sep="")
 print("Starting Enrichment..")
 t1<-Sys.time()
 round_count<-1
@@ -25,7 +25,7 @@ for (layer in layers){
     gene.stats.sorted <-  get.gene.stats.sorted(gene.stats)
     gene.subset <- get.gene.subset(gene.stats.sorted,index.range=1:100)
     result <- enrichment.wilcoxon(gene.subset,gene.data)
-    filename <- paste("output_wilcox_chr=",chr_t,"_layer=",layer,".csv",sep="")
+    filename <- paste("output_wilcox_chr=",chr_t,"_layer=",layer,sep="")
     output <- enrichment.output(result, gene.data, num_terms, filename, p_cutoff=0.05)
 
     t3<-Sys.time()
@@ -39,7 +39,7 @@ print(paste("Enrichment completed. Total Time elapsed: ",t4-t1," min",sep=""))
 # smallest 100 genes
 
 setwd("~/results_wilcox/small100")
-filename <- paste("output_wilcox_chr=",chr_t,".csv",sep="")
+filename <- paste("output_wilcox_chr=",chr_t,sep="")
 print("Starting Enrichment..")
 #t1<-Sys.time()
 round_count<-1
@@ -51,7 +51,7 @@ for (layer in layers){
     gene.stats.sorted <-  get.gene.stats.sorted(gene.stats)
     gene.subset <- get.gene.subset(gene.stats.sorted,index.range=(dim(gene.stats.sorted)[1]-100:dim(gene.stats.sorted)[1]))
     result <- enrichment.wilcoxon(gene.subset,gene.data)
-    filename <- paste("output_wilcox_chr=",chr_t,"_layer=",layer,".csv",sep="")
+    filename <- paste("output_wilcox_chr=",chr_t,"_layer=",layer,sep="")
     output <- enrichment.output(result, gene.data, num_terms, filename, p_cutoff=0.05)
 
     t3<-Sys.time()
@@ -67,7 +67,7 @@ print(paste("Enrichment completed. Total Time elapsed: ",t4-t1," min",sep=""))
 # smallest 100 genes
 
 setwd("~/results_wilcox/large100+small100")
-filename <- paste("output_wilcox_chr=",chr_t,".csv",sep="")
+filename <- paste("output_wilcox_chr=",chr_t,sep="")
 print("Starting Enrichment..")
 #t1<-Sys.time()
 round_count<-1
@@ -79,7 +79,7 @@ for (layer in layers){
     gene.stats.sorted <-  get.gene.stats.sorted(gene.stats)
     gene.subset <- get.gene.subset(gene.stats.sorted,index.range=(c((1:50),dim(gene.stats.sorted)[1]-50:dim(gene.stats.sorted)[1])))
     result <- enrichment.wilcoxon(gene.subset,gene.data)
-    filename <- paste("output_wilcox_chr=",chr_t,"_layer=",layer,".csv",sep="")
+    filename <- paste("output_wilcox_chr=",chr_t,"_layer=",layer,sep="")
     output <- enrichment.output(result, gene.data, num_terms, filename, p_cutoff=0.05)
 
     t3<-Sys.time()
