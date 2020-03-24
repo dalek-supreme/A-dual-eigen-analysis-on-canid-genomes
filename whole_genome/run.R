759470c315abe47c13dfd2f2b622edb2b01f5feb
Xingzhi Sun <xs2365@columbia.edu>
2020-03-24 12:24:06 +0800

    for background running

--- whole_genome/run.R
+++ whole_genome/run.R
@@ -286,3 +286,41 @@ enrichment.output <- function(enrichment.result, gene.data, num_terms=20, filena
         enrichment.output.two.sided(enrichment.result, gene.data, num_terms, filename, p_cutoff)
 }
 
+
+
+load("gene.db.Rdata")
+load("gene.stats.Rdata")
+
+num_layers <- 10
+layers <- seq(num_layers)
+num_terms <- 50
+
+t1 <- proc.time()
+print("Starting gene enrichment analysis...")
+t5<-proc.time()
+for (layer in layers)
+{
+    t3 <- proc.time()
+    gene.stats.layer <- gene.loadings.stats.get_layer(gene.stats,layer)
+    gene.stats.sorted <-  gene.stats.sort(gene.stats.layer)
+    gene.stats.positive <- gene.subset.pole_split(gene.stats.sorted,pole="positive")
+    gene.stats.negative <- gene.subset.pole_split(gene.stats.sorted,pole="negative")
+
+    result.positive <- enrichment.wilcoxon(gene.stats.positive,gene.db) # about 7 minutes
+    result.negative <- enrichment.wilcoxon(gene.stats.negative,gene.db)
+    setwd(paste("~/enrichment/",layer,sep=""))
+    save(result.positive,result.negative,file="result.Rdata")
+    setwd(paste("~/enrichment/",layer,"/positive",sep=""))
+    enrichment.output(result.positive, gene.db, num_terms, filename="wilcox", p_cutoff=0.05)
+    setwd(paste("~/enrichment/",layer,"/negative",sep=""))
+    enrichment.output(result.negative, gene.db, num_terms, filename="wilcox", p_cutoff=0.05)
+    #enrichment.output.all(result, gene.data, filename)
+    t4 <- proc.time()-t3
+    print(paste("Layer ",layer," finished. Time elapsed: ",t4[3],sep=""))
+    t6<-proc.time()-t5
+    print(paste("Estimated time remaining: ",t6[3]*(length(layers)/layer-1)/60,"minutes.",sep=""))
+}
+
+t7 <- proc.time()-t0
+print(paste("Analysis complete. Total time: ",t7[3],sep=""))
+
