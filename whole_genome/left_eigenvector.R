c56d3569c8879b771d429198a4eafe5e80a3c8c0
Xingzhi Sun <xs2365@columbia.edu>
2020-04-15 23:10:31 +0800

    Update left_eigenvector.R

--- whole_genome/left_eigenvector.R
+++ whole_genome/left_eigenvector.R
@@ -21,21 +21,22 @@ for (layer in seq(num_layers)) {
     write.csv(demographic.output,file=(paste('demographic_layer_',layer,'.csv',sep='')))
 }
 
-setwd("enrichment/demographic/")
-for (layer in seq(num_layers)) {
+setwd("enrichment_whole_genome/map")
+for (layer in 2:num_layers) {
     demographic.map <- demographic.data
     positivity <- sapply(snp.svd$u[,layer],
                                         function(x) {
                                             if (x<0) return(1)
                                             else if (x>=0) return(2)
                                         })
-    demographic.map$color <- factor(positivity,labels=c('negative','non-negative'))
+    demographic.map$color <- factor(positivity,labels=c('negative','positive'))
     demographic.map <- demographic.map[demographic.data$SampleID!='Lcu2_Pastora',]
-    pdf(paste('map_layer_',layer,'.pdf',sep=''))
     mapworld<-borders("world",colour = "gray50",fill="white")
     mp<-ggplot()+mapworld+ylim(-60,90)
     mp<-mp+geom_point(aes(x=as.numeric(demographic.map$Longitude),
-                          y=as.numeric(demographic.map$Latitude),color=as.factor(demographic.map$color)))
+                          y=as.numeric(demographic.map$Latitude),color=demographic.map$color))
+    #mp <- mp+theme(legend.text = element_text('Loading Positivity'))
+    pdf(file=paste('map_layer_',layer,'.pdf',sep=''),width = 10,height = 5)
     mp
     dev.off()
 }
