8504b9b3fb0e789683ae0647996ca599f973b6a4
Xingzhi Sun <xs2365@columbia.edu>
2020-04-15 22:45:40 +0800

    Update left_eigenvector.R

--- whole_genome/left_eigenvector.R
+++ whole_genome/left_eigenvector.R
@@ -8,6 +8,12 @@ for (i in seq_along(sample.id)) {
 
 demographic.data <- demographic.data[demographic.index,]
 
+library(ggplot2)
+library(ggmap)
+library(sp)
+library(maptools)
+library(maps)
+
 setwd("enrichment/demographic/")
 for (layer in seq(num_layers)) {
     canid.order <- order(snp.svd$u[,layer])
@@ -15,3 +21,21 @@ for (layer in seq(num_layers)) {
     write.csv(demographic.output,file=(paste('demographic_layer_',layer,'.csv',sep='')))
 }
 
+setwd("enrichment/demographic/")
+for (layer in seq(num_layers)) {
+    demographic.map <- demographic.data
+    positivity <- sapply(snp.svd$u[,layer],
+                                        function(x) {
+                                            if (x<0) return(1)
+                                            else if (x>=0) return(2)
+                                        })
+    demographic.map$color <- factor(positivity,labels=c('negative','non-negative'))
+    demographic.map <- demographic.map[demographic.data$SampleID!='Lcu2_Pastora',]
+    pdf(paste('map_layer_',layer,'.pdf',sep=''))
+    mapworld<-borders("world",colour = "gray50",fill="white")
+    mp<-ggplot()+mapworld+ylim(-60,90)
+    mp<-mp+geom_point(aes(x=as.numeric(demographic.map$Longitude),
+                          y=as.numeric(demographic.map$Latitude),color=as.factor(demographic.map$color)))
+    mp
+    dev.off()
+}
