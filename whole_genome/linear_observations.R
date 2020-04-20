53989d69a7a0847645f641dc27ad9f5d898ab0f6
Xingzhi Sun <xs2365@columbia.edu>
2020-04-20 18:27:12 +0800

    Update linear_observations.R

--- whole_genome/linear_observations.R
+++ whole_genome/linear_observations.R
@@ -341,3 +341,428 @@ for (i in seq(10)) {
     print(acos(sum(snp.svd$u[,i]*snp.recoding.svd$u[,i]))/pi*180)
 }
 
+for (i in seq(10)){
+    print(sum(order(snp.svd$u[,i])-order(snp.recoding.svd$u[,i])))
+}
+for (i in seq(10)){
+    print(sum(order(snp.svd$v[,i])-order(snp.recoding.svd$v[,i])))
+}
+
+# interestingly, no order has changed.
+
+A1<-snp.recoding.svd$d[1]*snp.recoding.svd$u[,1]%*%t(snp.recoding.svd$v[,1])
+
+A1.rowmax<-array()
+for (i in seq(dim(A1)[1])){
+    A1.rowmax[i]<-max(A1[i,])
+}
+
+A1.colmax<-array()
+for (j in seq(dim(A1)[2])){
+    A1.colmax[j]<-max(A1[,j])
+}
+
+
+snp.genotype.recoding.rowmax<-array()
+for (i in seq(dim(snp.genotype.recoding)[1])){
+    snp.genotype.recoding.rowmax[i]<-max(snp.genotype.recoding[i,])
+}
+
+snp.genotype.recoding.colmax<-array()
+for (j in seq(dim(snp.genotype.recoding)[2])){
+    snp.genotype.recoding.colmax[j]<-max(snp.genotype.recoding[,j])
+}
+
+
+setwd('~/observations/recoding')
+
+pdf(file=paste('Histogram-singlar-values.pdf',sep=''))
+hist(snp.recoding.svd$d)
+dev.off()
+num_layers <- 10
+for (layer in seq(num_layers)){
+    pdf(file=paste('Histogram-left-',layer,'.pdf',sep=''))
+    hist(snp.recoding.svd$u[,layer])
+    dev.off()
+    pdf(file=paste('Histogram-right-',layer,'.pdf',sep=''))
+    hist(snp.recoding.svd$v[,layer])
+    dev.off()
+}
+
+
+A1<-snp.recoding.svd$d[1]*snp.recoding.svd$u[,1]%*%t(snp.recoding.svd$v[,1])
+hist(A1)
+A2<-snp.recoding.svd$d[2]*snp.recoding.svd$u[,2]%*%t(snp.recoding.svd$v[,2])
+hist(A2)
+
+sort(abs(A1))[(length(A1)-100):length(A1)]
+sort(abs(A2))[(length(A2)-100):length(A2)]
+
+v2 <- filter.loadings(snp.svd$v[,2],method='2',norm.cutoff=0.9)
+count <- 0
+for (val in v2){
+    if (val != 0) {
+        count <- count +1
+    }
+}
+count
+count/length(v2)
+
+for (layer in seq(num_layers)){
+    v2 <- filter.loadings(snp.svd$v[,layer],method='2',norm.cutoff=0.9)
+    count <- 0
+    for (val in v2){
+        if (val != 0) {
+            count <- count +1
+        }
+    }
+    print(count)
+    print(count/length(v2))
+}
+
+for (layer in seq(num_layers)){
+    v2 <- filter.loadings(snp.svd$u[,layer],method='2',norm.cutoff=0.9)
+    count <- 0
+    for (val in v2){
+        if (val != 0) {
+            count <- count +1
+        }
+    }
+    print(count)
+    print(count/length(v2))
+}
+
+
+
+
+
+
+num_layers<-10
+B <- array(0,dim(snp.genotype))
+for (layer in seq(num_layers)){
+    u <- filter.loadings(snp.svd$u[,layer],method='2',norm.cutoff=0.99)
+    v <- filter.loadings(snp.svd$v[,layer],method='2',norm.cutoff=0.99)
+    B <- B + snp.svd$d[layer]*u %*% t(v)
+}
+
+
+err <- norm(snp.genotype-B, type='2')
+
+A1 <- snp.svd$u[,1]%*%t(snp.svd$v[,1])
+u <- filter.loadings(snp.svd$u[,1],method='2',norm.cutoff=0.9)
+v <- filter.loadings(snp.svd$v[,1],method='2',norm.cutoff=0.9)
+B1 <- u %*% t(v)
+
+norm(A1-B1,type='2')
+
+A2 <- snp.svd$u[,2]%*%t(snp.svd$v[,2])
+u <- filter.loadings(snp.svd$u[,2],method='2',norm.cutoff=0.9)
+v <- filter.loadings(snp.svd$v[,2],method='2',norm.cutoff=0.9)
+B2 <- u %*% t(v)
+
+norm(A2-B2,type='2')
+
+A3 <- snp.svd$u[,3]%*%t(snp.svd$v[,3])
+u <- filter.loadings(snp.svd$u[,3],method='2',norm.cutoff=0.9)
+v <- filter.loadings(snp.svd$v[,3],method='2',norm.cutoff=0.9)
+B3 <- u %*% t(v)
+
+norm(A3-B3,type='2')
+
+A4 <- snp.svd$u[,4]%*%t(snp.svd$v[,4])
+u <- filter.loadings(snp.svd$u[,4],method='2',norm.cutoff=0.9)
+v <- filter.loadings(snp.svd$v[,4],method='2',norm.cutoff=0.9)
+B4 <- u %*% t(v)
+
+norm(A4-B4,type='2')
+
+
+for (layer in seq(num_layers)){
+    u <- filter.loadings(snp.svd$u[,layer],method='2',norm.cutoff=0.7)
+    v <- filter.loadings(snp.svd$v[,layer],method='2',norm.cutoff=0.7)
+    count0 <- 0
+    for (i in u) {
+        if (i!=0) count0 <- count0+1
+    }
+    print(paste(layer,'u',count0))
+    count0 <- 0
+    for (i in v) {
+        if (i!=0) count0 <- count0+1
+    }
+    print(paste(layer,'v',count0))
+}
+
+num_layers<-10
+B <- snp.svd$d[1]*snp.svd$u[,1]%*%t(snp.svd$v[,1])
+for (layer in 2:num_layers){
+    u <- filter.loadings(snp.svd$u[,layer],method='2',norm.cutoff=0.7)
+    v <- filter.loadings(snp.svd$v[,layer],method='2',norm.cutoff=0.7)
+    B <- B + snp.svd$d[layer]*u %*% t(v)
+}
+err <- norm(snp.genotype-B, type='2')
+
+for (layer in seq(num_layers)){
+    u <- filter.loadings(snp.svd$u[,layer],method='2',norm.cutoff=0.6)
+    v <- filter.loadings(snp.svd$v[,layer],method='2',norm.cutoff=0.6)
+    count0 <- 0
+    for (i in u) {
+        if (i!=0) count0 <- count0+1
+    }
+    print(paste(layer,'u',count0))
+    count0 <- 0
+    for (i in v) {
+        if (i!=0) count0 <- count0+1
+    }
+    print(paste(layer,'v',count0))
+}
+
+num_layers<-10
+B <- snp.svd$d[1]*snp.svd$u[,1]%*%t(snp.svd$v[,1])
+for (layer in 2:num_layers){
+    u <- filter.loadings(snp.svd$u[,layer],method='2',norm.cutoff=0.6)
+    v <- filter.loadings(snp.svd$v[,layer],method='2',norm.cutoff=0.6)
+    B <- B + snp.svd$d[layer]*u %*% t(v)
+}
+err <- norm(snp.genotype-B, type='2')
+
+
+for (layer in seq(num_layers)){
+    u <- filter.loadings(snp.svd$u[,layer],method='2',norm.cutoff=0.5)
+    v <- filter.loadings(snp.svd$v[,layer],method='2',norm.cutoff=0.5)
+    count0 <- 0
+    for (i in u) {
+        if (i!=0) count0 <- count0+1
+    }
+    print(paste(layer,'u',count0))
+    count0 <- 0
+    for (i in v) {
+        if (i!=0) count0 <- count0+1
+    }
+    print(paste(layer,'v',count0))
+}
+
+num_layers<-10
+B <- snp.svd$d[1]*snp.svd$u[,1]%*%t(snp.svd$v[,1])
+for (layer in 2:num_layers){
+    u <- filter.loadings(snp.svd$u[,layer],method='2',norm.cutoff=0.5)
+    v <- filter.loadings(snp.svd$v[,layer],method='2',norm.cutoff=0.5)
+    B <- B + snp.svd$d[layer]*u %*% t(v)
+}
+err <- norm(snp.genotype-B, type='2')
+
+cutoff <- 0.4
+
+num_layers<- 8
+u.cutoff <- 0.8
+v.cutoff <- 0.8
+for (layer in seq(num_layers)){
+    u <- filter.loadings(snp.svd$u[,layer],method='2',norm.cutoff=u.cutoff)
+    v <- filter.loadings(snp.svd$v[,layer],method='2',norm.cutoff=v.cutoff)
+    count0 <- 0
+    for (i in u) {
+        if (i!=0) count0 <- count0+1
+    }
+    print(paste(layer,'u',count0))
+    count0 <- 0
+    for (i in v) {
+        if (i!=0) count0 <- count0+1
+    }
+    print(paste(layer,'v',count0))
+}
+
+B <- snp.svd$d[1]*snp.svd$u[,1]%*%t(snp.svd$v[,1])
+for (layer in 2:num_layers){
+    u <- filter.loadings(snp.svd$u[,layer],method='2',norm.cutoff=u.cutoff)
+    v <- filter.loadings(snp.svd$v[,layer],method='2',norm.cutoff=v.cutoff)
+    B <- B + snp.svd$d[layer]*u %*% t(v)
+}
+err <- norm(snp.genotype-B, type='2')
+
+
+for (i in 1:10) {
+    u.cutoff <- 1-0.02*i -> v.cutoff
+    print(u.cutoff)
+    for (layer in seq(num_layers)){
+        u <- filter.loadings(snp.svd$u[,layer],method='2',norm.cutoff=u.cutoff)
+        v <- filter.loadings(snp.svd$v[,layer],method='2',norm.cutoff=v.cutoff)
+        count0 <- 0
+        for (i in u) {
+            if (i!=0) count0 <- count0+1
+        }
+        print(paste(layer,'u',count0))
+        count0 <- 0
+        for (i in v) {
+            if (i!=0) count0 <- count0+1
+        }
+        print(paste(layer,'v',count0))
+    }
+
+    num_layers<-10
+    B <- snp.svd$d[1]*snp.svd$u[,1]%*%t(snp.svd$v[,1])
+    for (layer in 2:num_layers){
+        u <- filter.loadings(snp.svd$u[,layer],method='2',norm.cutoff=u.cutoff)
+        v <- filter.loadings(snp.svd$v[,layer],method='2',norm.cutoff=v.cutoff)
+        B <- B + snp.svd$d[layer]*u %*% t(v)
+    }
+    err <- norm(snp.genotype-B, type='2')
+    print(err)
+}
+
+u.cutoff <- 0.8 -> v.cutoff
+for (num_layers in 1:10){
+    B <- snp.svd$d[1]*snp.svd$u[,1]%*%t(snp.svd$v[,1])
+    for (layer in 2:num_layers){
+        u <- filter.loadings(snp.svd$u[,layer],method='2',norm.cutoff=u.cutoff)
+        v <- filter.loadings(snp.svd$v[,layer],method='2',norm.cutoff=v.cutoff)
+        B <- B + snp.svd$d[layer]*u %*% t(v)
+    }
+    err <- norm(snp.genotype-B, type='2')
+    print(paste(num_layers,err))
+}
+
+C <- array(2,dim(snp.genotype))
+norm(C,type='2')
+2*sqrt(dim(snp.genotype)[1]*dim(snp.genotype)[2])
+
+norm(C-snp.svd$d[1]*snp.svd$u[,1]%*%t(snp.svd$v[,1]),type='2')
+# 1604.177 
+
+norm(C-snp.svd$d[1]*snp.svd$u[,1]%*%t(snp.svd$v[,1])
+      -snp.svd$d[2]*snp.svd$u[,2]%*%t(snp.svd$v[,2])
+      ,type='2')
+# 1604.431
+
+norm(C-snp.svd$d[1]*snp.svd$u[,1]%*%t(snp.svd$v[,1])
+      -snp.svd$d[2]*snp.svd$u[,2]%*%t(snp.svd$v[,2])
+      -snp.svd$d[3]*snp.svd$u[,3]%*%t(snp.svd$v[,3])
+      ,type='2')
+# 1604.435
+
+order(snp.old.svd$u[,1])==order(snp.recoding.svd$u[,1])
+
+num_layers<- 8
+u.cutoff <- 0.8
+v.cutoff <- 0.8
+for (layer in seq(num_layers)){
+    u <- filter.loadings(snp.svd$u[,layer],method='2',norm.cutoff=u.cutoff)
+    v <- filter.loadings(snp.svd$v[,layer],method='2',norm.cutoff=v.cutoff)
+    print(paste(layer,'u'))
+    print(nonzero.pos(u))
+}
+for (layer in seq(num_layers)){
+    u <- filter.loadings(snp.svd$u[,layer],method='2',norm.cutoff=u.cutoff)
+    v <- filter.loadings(snp.svd$v[,layer],method='2',norm.cutoff=v.cutoff)
+    print(paste(layer,'v'))
+    print(nonzero.pos(v))
+}
+
+cor_mat <- array(0,c(num_layers,num_layers))
+for (i in seq(num_layers-1)){
+    for (j in (i+1):num_layers){
+        u1 <- filter.loadings(snp.svd$u[,i],method='2',norm.cutoff=u.cutoff)
+        u2 <- filter.loadings(snp.svd$u[,j],method='2',norm.cutoff=u.cutoff)
+        cor_mat[i,j]<-cor(u1,u2)
+    }
+}
+
+cor_mat <- array(0,c(num_layers,num_layers))
+for (i in seq(num_layers-1)){
+    for (j in (i+1):num_layers){
+        u1 <- filter.loadings(snp.svd$u[,i],method='2',norm.cutoff=u.cutoff)
+        u2 <- filter.loadings(snp.svd$u[,j],method='2',norm.cutoff=u.cutoff)
+        cor_mat[i,j]<-cor(order(u1),order(u2))
+    }
+}
+
+inter_mat <- array(0,c(num_layers,num_layers))
+for (i in seq(num_layers-1)){
+    for (j in (i+1):num_layers){
+        u1 <- filter.loadings(snp.svd$u[,i],method='2',norm.cutoff=u.cutoff)
+        u2 <- filter.loadings(snp.svd$u[,j],method='2',norm.cutoff=u.cutoff)
+        inter_mat[i,j]<-length(intersect(nonzero.pos(u1),nonzero.pos(u2)))
+    }
+}
+
+u.inter_rate_mat <- array(0,c(num_layers,num_layers))
+for (i in seq(num_layers-1)){
+    for (j in (i+1):num_layers){
+        u1 <- filter.loadings(snp.svd$u[,i],method='2',norm.cutoff=u.cutoff)
+        u2 <- filter.loadings(snp.svd$u[,j],method='2',norm.cutoff=u.cutoff)
+        u.inter_rate_mat[i,j]<-length(intersect(nonzero.pos(u1),nonzero.pos(u2)))/length(union(nonzero.pos(u1),nonzero.pos(u2)))
+    }
+}
+v.inter_rate_mat <- array(0,c(num_layers,num_layers))
+for (i in seq(num_layers-1)){
+    for (j in (i+1):num_layers){
+        v1 <- filter.loadings(snp.svd$v[,i],method='2',norm.cutoff=v.cutoff)
+        v2 <- filter.loadings(snp.svd$v[,j],method='2',norm.cutoff=v.cutoff)
+        v.inter_rate_mat[i,j]<-length(intersect(nonzero.pos(v1),nonzero.pos(v2)))/length(union(nonzero.pos(v1),nonzero.pos(v2)))
+    }
+}
+
+
+
+
+# MLE of bernouli probability:
+count.0 <- 0
+count.1 <- 0
+count.2 <- 0
+
+for (i in seq(dim(snp.genotype)[1])){
+    for (j in seq(dim(snp.genotype)[2])){
+        if (snp.genotype[i,j]==0)
+            count.0 <- count.0+1
+        else if (snp.genotype[i,j]==1)
+            count.1 <- count.1+1
+        else if (snp.genotype[i,j]==2)
+            count.2 <- count.2+1
+        else print(snp.genotype[i,j])
+    }
+}
+
+p <- (count.1+2*count.0)/(2*(count.0+count.1+count.2))
+n <- 2*(count.0+count.1+count.2)
+derivative5 <- snp.svd$u[,5]%*%t(snp.svd$v[,5])
+
+k <- (snp.svd$d[5]-snp.svd$d[6])/(mean(derivative5)*(2*count.0+count.1)/(count.0+count.1))
+2*(1-pnorm(k/sqrt(2*n*p*(1-p))))
+# =0
+
+k <- (snp.svd$d[5]-snp.svd$d[6])/(max(derivative5)*(2*count.0+count.1)/(count.0+count.1))
+2*(1-pnorm(k/sqrt(2*n*p*(1-p))))
+
+plot(sort(derivative5,decreasing=T)[1:10000])
+
+
+num_layers<- 8
+u.thrshld <- 3/sqrt(length(snp.svd$u[,1]))
+v.thrshld <- 3/sqrt(length(snp.svd$v[,1]))
+for (layer in seq(num_layers)){
+    u <- filter.loadings(snp.svd$u[,layer],method='threshold',threshold=u.thrshld)
+    v <- filter.loadings(snp.svd$v[,layer],method='threshold',threshold=v.thrshld)
+    print(paste(layer,'u'))
+    print(nonzero.pos(u))
+}
+for (layer in seq(num_layers)){
+    u <- filter.loadings(snp.svd$u[,layer],method='threshold',threshold=u.thrshld)
+    v <- filter.loadings(snp.svd$v[,layer],method='threshold',threshold=v.thrshld)
+    print(paste(layer,'v'))
+    print(nonzero.pos(v))
+}
+
+
+num_layers<- 8
+u.cutoff <- 0.8
+v.cutoff <- 0.8
+for (layer in seq(num_layers)){
+    u <- filter.loadings(snp.svd$u[,layer],method='2',norm.cutoff=u.cutoff)
+    #v <- filter.loadings(snp.svd$v[,layer],method='2',norm.cutoff=v.cutoff)
+    print(paste(layer,'u'))
+    print(u[nonzero.pos(u)])
+}
+for (layer in seq(num_layers)){
+    #u <- filter.loadings(snp.svd$u[,layer],method='2',norm.cutoff=u.cutoff)
+    v <- filter.loadings(snp.svd$v[,layer],method='2',norm.cutoff=v.cutoff)
+    print(paste(layer,'v'))
+    print(v[nonzero.pos(v)])
+}
