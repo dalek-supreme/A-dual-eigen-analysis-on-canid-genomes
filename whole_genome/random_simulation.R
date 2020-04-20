6e7ff9cfc8ef786b8a6ea54f73a7ca4decbf27c3
Xingzhi Sun <xs2365@columbia.edu>
2020-04-20 19:38:50 +0800

    Update random_simulation.R

--- whole_genome/random_simulation.R
+++ whole_genome/random_simulation.R
@@ -1,3 +1,6 @@
+load('snp.genotype.Rdata')
+
+# get MLE 
 count <- array(0,3)
 names(count) <- c(0,1,2)
 for (i in seq(dim(snp.genotype)[1])){
@@ -11,15 +14,32 @@ for (i in seq(dim(snp.genotype)[1])){
         else print(snp.genotype[i,j])
     }
 }
+#generate
 probab <- count/sum(count)
 rand.value <- array(runif(dim(snp.genotype)[1]*dim(snp.genotype)[2]),dim(snp.genotype))
-rand.genotype <- array(0,dim(snp.genotype))
-for (i in dim(snp.genotype)[1]){
-    for (j in dim(snp.genotype)[2]){
-        if (rand.value[i][j] < probab[1]) rand.genotype[i][j] <- 0
-        else if (rand.value[i][j] < prabab[2]) rand.genotype[i][j] <- 1
-        else rand.genotype[i][j] <- 2
+rand.genotype <- array(-1,dim(snp.genotype))
+for (i in seq(dim(snp.genotype)[1])){
+    for (j in seq(dim(snp.genotype)[2])){
+        if (rand.value[i,j] < probab[1]) rand.genotype[i,j] <- 0
+        else if (rand.value[i,j] < probab[1]+probab[2]) rand.genotype[i,j] <- 1
+        else rand.genotype[i,j] <- 2
     }
 }
-
+#check
+rand.count <- array(0,3)
+names(rand.count) <- c(0,1,2)
+for (i in seq(dim(rand.genotype)[1])){
+    for (j in seq(dim(rand.genotype)[2])){
+        if (rand.genotype[i,j]==0)
+            rand.count[1] <- rand.count[1]+1
+        else if (rand.genotype[i,j]==1)
+            rand.count[2] <- rand.count[2]+1
+        else if (rand.genotype[i,j]==2)
+            rand.count[3] <- rand.count[3]+1
+        else print(rand.genotype[i,j])
+    }
+}
+rand.probab <- rand.count/sum(rand.count)
+(rand.probab - probab)/probab
+# svd
 rand.svd <- svd(rand.genotype)
