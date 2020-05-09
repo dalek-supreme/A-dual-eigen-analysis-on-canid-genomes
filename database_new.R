f7a2fb33d2442d6e0d1ec88aecf7fd691959f80f
Xingzhi Sun <xs2365@columbia.edu>
2020-05-09 23:34:44 +0800

    Create database_new.R

---
+++ database_new.R
@@ -0,0 +1,27 @@
+library(GO.db)
+library(KEGGREST)
+
+# KEGG
+gene2pathway.genes <- keggLink('cfa','pathway')
+gene2pathway.pathways <- keggLink('pathway','cfa')
+gene2pathway.ncbi_ids <- keggConv("ncbi-geneid",gene2pathway.genes)
+
+gene2pathway.pathways <- gene2pathway.pathways[names(gene2pathway.ncbi_ids)]
+
+KEGGPATHID2EXTID <- data.frame(
+    from=gene2pathway.pathways,
+    to=as.numeric(gsub('ncbi-geneid:','',gene2pathway.ncbi_ids))
+)
+rownames(KEGGPATHID2EXTID) <- NULL
+
+pathway.name <- keggList('pathway','cfa')
+KEGGPATHID2NAME <- data.frame(
+     from=names(pathway.name),
+     to=pathway.name
+    )
+rownames(KEGGPATHID2NAME) <- NULL
+
+kegg.db <- list(KEGGPATHID2EXTID,KEGGPATHID2NAME)
+names(kegg.db) <- c('KEGGPATHID2EXTID','KEGGPATHID2NAME')
+
+save(kegg.db,'kegg.db.Rdata')
