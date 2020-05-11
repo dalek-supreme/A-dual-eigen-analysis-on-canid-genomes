da85e4bc11fd5b99bfb628fd3bf585fe51dabb27
Xingzhi Sun <xs2365@columbia.edu>
2020-05-11 17:02:10 +0800

    KEGG and reactome databases

--- database_new.R
+++ database_new.R
@@ -1,4 +1,4 @@
-library(GO.db)
+#library(GO.db)
 library(KEGGREST)
 
 # KEGG
@@ -11,7 +11,7 @@ gene2pathway.pathways <- gene2pathway.pathways[names(gene2pathway.ncbi_ids)]
 KEGGPATHID2EXTID <- data.frame(
     from=gene2pathway.pathways,
     to=as.numeric(gsub('ncbi-geneid:','',gene2pathway.ncbi_ids)),
-    string.as.factor=F
+    stringsAsFactors=F
 )
 rownames(KEGGPATHID2EXTID) <- NULL
 
@@ -19,7 +19,7 @@ pathway.name <- keggList('pathway','cfa')
 KEGGPATHID2NAME <- data.frame(
      from=names(pathway.name),
      to=pathway.name,
-     string.as.factor=F
+     stringsAsFactors=F
     )
 rownames(KEGGPATHID2NAME) <- NULL
 
@@ -27,3 +27,14 @@ kegg.db <- list(KEGGPATHID2EXTID,KEGGPATHID2NAME)
 names(kegg.db) <- c('KEGGPATHID2EXTID','KEGGPATHID2NAME')
 
 save(kegg.db,file='kegg.db.Rdata')
+
+# reactome
+library(reactome.db)
+
+REACTOMEPATHID2EXTID <- as.data.frame(reactomePATHID2EXTID)
+names(REACTOMEPATHID2EXTID) <- c('from','to')
+REACTOMEPATHID2NAME <- as.data.frame(reactomePATHID2NAME)
+names(REACTOMEPATHID2NAME) <- c('from','to')
+REACTOME.db <- list(REACTOMEPATHID2EXTID,REACTOMEPATHID2NAME)
+names(REACTOME.db) <- c('REACTOMEPATHID2EXTID','REACTOMEPATHID2NAME')
+save(REACTOME.db,file='REACTOME.db.Rdata')
